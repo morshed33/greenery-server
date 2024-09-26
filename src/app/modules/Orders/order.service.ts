1dc83dcda4797c7d1afab409017b8b136be3fb8b
morshed33 <ac3m0r@gmail.com>
2024-09-27 00:56:53 +0600

    order simplified

--- src/app/modules/Orders/order.service.ts
+++ src/app/modules/Orders/order.service.ts
@@ -1,22 +1,14 @@
 import { ProductService } from "../Products/product.service"
 import { IOrder } from "./order.interface"
 import { Order } from "./order.model"
-import Stripe from "stripe"
-
-// Initialize Stripe
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
-})
 
 const createOrder = async (orderData: Partial<IOrder>) => {
-  if (!orderData.customer || !orderData.products || !orderData.paymentMethod) {
+  if (!orderData.customer || !orderData.products) {
     throw new Error("Invalid order data")
   }
 
-  let totalAmount = 0
-
   for (const product of orderData.products) {
-    if (!product.productId || !product.quantity) {
+    if (!product.productId || !product.quantity || !product.price) {
       throw new Error("Invalid product data")
     }
 
@@ -32,33 +24,12 @@ const createOrder = async (orderData: Partial<IOrder>) => {
     productData.quantity -= product.quantity
     await productData.save()
 
-    totalAmount += productData.price * product.quantity
-    product.price = productData.price // Set price for the order
+    product.price = productData.price
+
+    orderData.totalAmount ??= 0
+    orderData.totalAmount += productData.price * product.quantity
   }
 
-  // Create a payment intent with Stripe
-  let paymentIntent
-  if (orderData.paymentMethod === "Stripe") {
-    console.log("Entering Stripe payment method")
-    paymentIntent = await stripe.paymentIntents.create({
-      amount: Math.round(totalAmount * 100), // Amount in cents
-      currency: "usd", // Change to your currency
-      payment_method_types: ["card"],
-    })
-
-    console.log("Payment intent created successfully:", paymentIntent)
-
-    // You may want to send the client secret back to the frontend for further processing
-    orderData.stripePaymentIntent = {
-      stripePaymentIntentId: paymentIntent.id,
-      status: paymentIntent.status,
-      clientSecret: paymentIntent.client_secret!,
-    }
-    console.log("Order data with Stripe payment intent:", orderData)
-  }
-
-  // Set the total amount and create the order
-  orderData.totalAmount = totalAmount // Set calculated total amount
   const newOrder = await Order.create(orderData)
   return newOrder
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,7 @@ dotenv.config({ path: path.join(process.cwd(), ".env") })
 
 export default {
   port: process.env.PORT,
-  origin: process.env.ORIGIN || "http://localhost:5173",
+  origin: process.env.ORIGIN || "https://gc-lime.vercel.app/",
   nodeEnv: process.env.NODE_ENV,
   dbUrl: process.env.DB_URL,
   dbName: process.env.DB_NAME || "",
